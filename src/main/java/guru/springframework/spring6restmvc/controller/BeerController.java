8b69360d646207c2e4e5ce38ce8c896fd0684910
Fady Zaafrane <fady.zaafrane@gmail.com>
2024-07-08 13:53:19 +0100

    add list beers

---
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -0,0 +1,30 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.model.Beer;
+import guru.springframework.spring6restmvc.service.BeerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+public class BeerController {
+    @Autowired
+    private BeerService beerService;
+
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers(){
+        return beerService.listBeers();
+    }
+
+    public Beer getBeerById(UUID id){
+        log.debug("Get Beer by Id - in controller");
+        return beerService.getBeerById(id);
+    }
+
+}
